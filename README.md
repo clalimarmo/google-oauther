28ee79c084c221aafba9dc9fabc8a83588a7edc1
Carlos Lalimarmo <clalimarmo@gmail.com>
2014-12-09 17:10:09 -0500

    initial commit
    factored out of another project

---
+++ README.md
@@ -0,0 +1,51 @@
+# google-oauther
+
+Simple Google OAuth2 module, for use in single page, client-side only
+applications.  It opens a popup window to request permission, and stores the
+auth token returned by Google.
+
+## Usage
+
+    var authenticator = require('google-oauther');
+
+    authenticator.onAuthenticate(function(auth) {
+      //stuff to do once the authenticator has authenticated user's profile info
+      //(The auth parameter passed to the callback the authenticator, itself)
+    });
+
+    authenticator.run({
+      scope: ['https://www.googleapis.com/auth/devstorage.read_write'],
+      clientID: 'your-google-oauth2-client-id'
+    });
+
+## Methods
+
+### onAuthenticate
+
+Specify behavior that should occur once the user grants the requested
+permissions. Do this before you call `run`
+
+### run
+
+Requests authentication from the user, and whatever permissions you specify
+with the `scope` parameter.
+
+### user
+
+Returns the user profile information gleaned from an authenticated request to
+https://www.googleapis.com/plus/v1/people/me
+
+### token
+
+Returns the auth token. Useful for subsequent requests to other Google APIs.
+
+## Notes
+
+The authenticator always requests the 'profile' OAuth2 scope, in addition to
+whatever scopes are specified in the call to `run`.
+
+For the popup window to send the auth token returned by google back to the main
+window, a global callback function is attached to the main window, called
+`authenticatedWith`. Including the `google-oauther` module will attach this
+callback function automatically; overwriting this function will break the
+authenticator.
---
+++ dist/google-oauther.js
@@ -0,0 +1,101 @@
+define(function(require) {
+
+  var $ = require('jquery');
+
+  var queryParams = function() {
+    // from https://developers.google.com/accounts/docs/OAuth2UserAgent
+    var params = {}, queryString = window.location.hash.substring(1),
+        regex = /([^&=]+)=([^&]*)/g, match;
+    match = regex.exec(queryString);
+    while (match) {
+      params[decodeURIComponent(match[1])] = decodeURIComponent(match[2]);
+      match = regex.exec(queryString);
+    }
+    return params;
+  }
+
+  //constructor dependency injection helper
+  var ensure = function(dependencyNames, dependencies) {
+    var onlyDependencies = {};
+    for (var i = 0; i < dependencyNames.length; i++) {
+      var expectedDependency = dependencyNames[i];
+      var injectedDependency = dependencies[expectedDependency];
+      if (injectedDependency === undefined || injectedDependency === null) {
+        throw new Error('missing dependency:' + expectedDependency);
+      } else {
+        onlyDependencies[expectedDependency] = injectedDependency;
+      }
+    }
+    return onlyDependencies;
+  };
+
+  var OAUTH2_ENDPOINT = 'https://accounts.google.com/o/oauth2/auth';
+  var PROFILE_ENDPOINT = 'https://www.googleapis.com/plus/v1/people/me';
+  var PROFILE_SCOPE = 'profile';
+
+  var token;
+  var user;
+  var onAuthenticateCallbacks = [];
+
+  var singleton = {};
+  var scope;
+
+  var fetchUserInformation = function() {
+    $.ajax({
+      url: PROFILE_ENDPOINT + '?access_token=' + token,
+      success: function(data) {
+        user = {
+          id: data.id,
+          displayName: data.displayName,
+          image: data.image,
+        };
+        for (var i = 0; i < onAuthenticateCallbacks.length; i++) {
+          onAuthenticateCallbacks[i](singleton);
+        }
+      },
+    });
+  };
+
+  // callback for popup - save token and get user info
+  window.authenticatedWith = function(_token) {
+    token = _token;
+    fetchUserInformation();
+  };
+
+  singleton.run = function(_config) {
+    ensure(['scope', 'clientID'], _config);
+
+    var config = _config;
+    scope = [PROFILE_SCOPE].concat(config.scope);
+
+    if (!queryParams.access_token) {
+      var url = OAUTH2_ENDPOINT +
+        '?scope=' + scope +
+        '&response_type=token'
+        '&redirect_uri=' + window.location.href +
+        '&client_id=' + config.clientID;
+      window.open(url, 'oauth', 'top=200,left=200,height=400,width=600');
+    } else {
+      window.opener.authenticatedWith(queryParams.access_token);
+      window.close();
+    }
+  };
+
+  singleton.isAuthenticated = function() {
+    return token !== undefined;
+  };
+
+  singleton.onAuthenticate = function(callback) {
+    onAuthenticateCallbacks.push(callback);
+  };
+
+  singleton.token = function() {
+    return token;
+  };
+
+  singleton.user = function() {
+    return user;
+  };
+
+  return singleton;
+});
